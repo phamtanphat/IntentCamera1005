37f26dd96b39f70569c32feedf2a2221656b3212
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-05 14:01:15 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/intentcamera1005/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/intentcamera1005/MainActivity.java
@@ -4,8 +4,11 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -19,6 +22,7 @@ public class MainActivity extends Activity {
     ImageView img;
 
     int Request_Camera = 123;
+    int Request_Gallery = 234;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +42,16 @@ public class MainActivity extends Activity {
                         Request_Camera);
             }
         });
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        Request_Gallery);
+            }
+        });
     }
 
     // Đáp trả việc xác thực cho phép sử dụng quyền của người dùng
@@ -49,13 +63,21 @@ public class MainActivity extends Activity {
                 startActivityForResult(intent , Request_Camera);
             }
         }
+        if (requestCode == Request_Gallery){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent , Request_Camera);
+            }
+        }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == Request_Camera && resultCode == RESULT_OK && data != null){
-            Log.d("BBB",data.toString() + "");
+            //Bitmap kiểu dữ liệu thông dụng của hình ảnh
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            img.setImageBitmap(bitmap);
         }
         super.onActivityResult(requestCode, resultCode, data);
     }

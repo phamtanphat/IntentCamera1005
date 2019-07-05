35e25572f40c5c68da564d5eee920a75a58882ae
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-05 13:05:33 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/intentcamera1005/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/intentcamera1005/MainActivity.java
@@ -1,13 +1,35 @@
 package khoapham.ptp.phamtanphat.intentcamera1005;
 
+import android.Manifest;
+import android.app.Activity;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends Activity {
+    Button btnCamera,btnGallery;
+    ImageView img;
+    int Request_Camera = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCamera = findViewById(R.id.buttonCamera);
+        btnGallery = findViewById(R.id.buttonGallery);
+        img = findViewById(R.id.imageview);
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(
+                        MainActivity.this,
+                        new String[]{Manifest.permission.CAMERA},
+                        Request_Camera);
+            }
+        });
     }
 }
